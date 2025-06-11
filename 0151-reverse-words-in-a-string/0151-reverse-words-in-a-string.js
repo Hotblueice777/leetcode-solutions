584b11d5a6b73dd1098b883b3b38f9613484b675
Hotblueice777 <157187883+Hotblueice777@users.noreply.github.com>
2025-06-10 23:06:23 -0600

    Time: 1 ms (83.66%), Space: 54.8 MB (63.52%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+
+    let words = s.trim().split(/\s+/); 
+    words.reverse();
+
+    return words.join(' ');
+};
+    
