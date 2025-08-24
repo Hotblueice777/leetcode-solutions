c523cb7db36f8d9f61ad728d25dc27312d4edd1a
Hotblueice777 <157187883+Hotblueice777@users.noreply.github.com>
2025-08-24 02:28:23 -0600

    Time: 19 ms (93.79%), Space: 71.1 MB (73.26%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+  const window = new Set();   
+             
+  for (let i = 0; i < nums.length; i++) {
+    if (window.has(nums[i])) return true; 
+    window.add(nums[i]);
+
+    if (i >= k) window.delete(nums[i - k]);
+  }
+  return false;
+};
