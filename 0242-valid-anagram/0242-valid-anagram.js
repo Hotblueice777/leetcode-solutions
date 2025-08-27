9088536eb44dd01442a99c30098b3758ac039aba
Hotblueice777 <157187883+Hotblueice777@users.noreply.github.com>
2025-08-27 03:51:16 -0600

    Time: 6 ms (93.11%), Space: 54.1 MB (99.45%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+        if (s.length !== t.length) return false; 
+
+    const count = new Array(26).fill(0); 
+
+    for (let i = 0; i < s.length; i++) {
+        count[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+        count[t.charCodeAt(i) - 'a'.charCodeAt(0)]--;
+    }
+
+    for (let c of count) {
+        if (c !== 0) return false;
+    }
+    return true;
+};
